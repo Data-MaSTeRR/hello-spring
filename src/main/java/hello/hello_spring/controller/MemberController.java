a0aa18c046beeefe471b32bb19361d1be6d6dc66
Eric SHIM <163520887+Data-MaSTeRR@users.noreply.github.com>
2025-10-04 23:37:22 +0900

    Feat: 회원등록 폼 만들어서 @PostMapping으로 입력받고, Service에 join 시키기

--- src/main/java/hello/hello_spring/controller/MemberController.java
+++ src/main/java/hello/hello_spring/controller/MemberController.java
@@ -1,9 +1,12 @@
 package hello.hello_spring.controller;
 
 
+import hello.hello_spring.domain.Member;
 import hello.hello_spring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 // Controller: 외부 요청 받기
 @Controller
@@ -15,6 +18,22 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    // 해당 URL에서, POST 요청에 대한 로직
+    // => MemberForm 객체로 입력값이 들어옴
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+        return "redirect:/"; // 회원 등록 후, 홈화면으로 리다이렉트
     }
 
 }
