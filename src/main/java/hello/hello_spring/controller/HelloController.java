4cfd802bd2b82105603db8dba2439ec7a7514a25
Eric SHIM <163520887+Data-MaSTeRR@users.noreply.github.com>
2025-10-04 18:23:49 +0900

    Feat: Controller로 API 응답 json형태

--- src/main/java/hello/hello_spring/controller/HelloController.java
+++ src/main/java/hello/hello_spring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -18,6 +19,32 @@ public class HelloController {
     public String helloMvc(@RequestParam("name") String name, Model model) {
         model.addAttribute("name", name);
         return "hello-template"; // hello-template.html로 가서 렌더링 "View Resolver"
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody // 따로 html을 내려주는 것이 아니라, return 값을 api로 응답 (String => stringConverter)
+    public String helloString(@RequestParam("name") String name) {
+        return "hello" + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody // 따로 html을 내려주는 것이 아니라, return 값을 api로 응답 (객체 => jsonConverter)
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    public static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 
 }
