a9a643cf68120c11a9d2655894eda452c63e4d8d
Eric SHIM <163520887+Data-MaSTeRR@users.noreply.github.com>
2025-10-04 18:06:01 +0900

    Feat: MVC 패턴 실습 (hello-template.html)

--- src/main/java/hello/hello_spring/controller/HelloController.java
+++ src/main/java/hello/hello_spring/controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hello_spring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -11,6 +12,12 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("data", "Eric!");
         return "hello"; // hello.html로 가서 렌더링 "View Resolver"
+    }
+
+    @GetMapping("hello-mvc") // GET요청 /hello-mvc 도메인으로 들어왔을 때,
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template"; // hello-template.html로 가서 렌더링 "View Resolver"
     }
 
 }
